41ebc2a809650ffb8394bc93115009b5ad6e8d00
WizzyWodich <wizzywodich@gmail.com>
2024-06-15 18:32:30 +0300

    Added method for leaving users for the server

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from disnake.ext import commands
 import os
 import sys
 from dotenv import load_dotenv
+from database.Database import UsersDataBase
 
 load_dotenv("config/config.env")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+global_db = UsersDataBase()
 intents = disnake.Intents.all()
 intents.message_content = True
 prefix = os.getenv('PREFIX')
 bot = commands.Bot(command_prefix=prefix, intents=intents, test_guilds=[1132400062450909244])
 bot.remove_command("help")
+
+@bot.event
+async def on_member_remove(member: disnake.Member):
+    await global_db.delete_user_from_all_databases(member.id)
+
 
 @bot.event
 async def on_ready():
