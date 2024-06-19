a76d63eb6e93919d1dbde52bd380b48d2cb7fbda
WizzyWodich <wizzywodich@gmail.com>
2024-06-19 18:56:55 +0300

    Added new module

--- cogs/user_card.py
+++ cogs/user_card.py
@@ -3,7 +3,7 @@ import disnake
 from disnake.ext import commands
 from database.RankDatabase import RankDatabase
 
-global_db = RankDatabase()
+rank_db = RankDatabase()
 
 class UserPanell(commands.Cog):
     def __init__(self, bot):
@@ -12,16 +12,15 @@ class UserPanell(commands.Cog):
     @commands.slash_command(name="mycard", description="Карточка пользователя")
     @commands.is_owner()
     async def mycard(self, interaction, member: disnake.Member = None):
-        await global_db.create_table()  
+        await rank_db.create_table()  
         if not member:
             member = interaction.author
-        await global_db.add_user(member)
-        user = await global_db.get_user(member)  
-        
+        await rank_db.add_user(member)
+        user = await rank_db.get_user(member)  
         
         embed = disnake.Embed(color=0xf24e4e, title=f'Карточка пользователя - {user[1]}')
         embed.add_field(name='<:Coin:1251937857782808586> Деньги', value=f'```{user[5]}```') 
-        embed.add_field(name='<:Ruby:1251937860672684163> Премиум', value=f'```{user[6]}```')  
+        embed.add_field(name='<:Ruby:1251937860672684163> Рубины', value=f'```{user[6]}```')  
         embed.add_field(name='<:ChevronUp:1251937855912280115> Уровень', value=f'```{user[2]}```')  
         embed.add_field(name='<:GlowingStar:1251937859263402145> Опыт', value=f'```{user[3]} / {user[4]}```')  
         embed.set_thumbnail(url=member.display_avatar.url)  
@@ -34,8 +33,9 @@ class UserPanell(commands.Cog):
         elif len(message.content) == 1:
             return
         else:
-            await global_db.update_level(message.author.id)   
-            await global_db.update_score(message.author.id)
+            await rank_db.add_user(message.author)
+            await rank_db.update_level(message.author.id)   
+            await rank_db.update_score(message.author.id)
     
 def setup(bot):
     bot.add_cog(UserPanell(bot))
