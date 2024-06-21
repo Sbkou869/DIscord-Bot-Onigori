3daa60dbaddd527aec2e1871ba1ac1774065a927
Wizzazi <155719588+WizzyWodich@users.noreply.github.com>
2024-06-21 10:36:15 +0300

    Modify file

--- cogs/admin_panel.py
+++ cogs/admin_panel.py
@@ -5,7 +5,9 @@ import datetime
 from disnake.ui.action_row import ModalUIComponent
 from disnake.utils import MISSING
 from database.UserInfoDatabase import UsersDataBase
+from database.LogsDatabase import LogsDatabase
 
+log_db = LogsDatabase()
 user_db = UsersDataBase()
 
 class ModalDeleteWarn(disnake.ui.Modal):
@@ -35,7 +37,7 @@ class ModalDeleteWarn(disnake.ui.Modal):
             embed.set_thumbnail(url=interaction.author.display_avatar.url)
             await interaction.response.send_message(embed=embed, ephemeral=True)
             
-            channel = interaction.guild.get_channel(await user_db.get_log_channel(interaction.guild))  # Вставить ID канала куда будут отправляться заявки
+            channel = interaction.guild.get_channel(await log_db.get_log_channel(interaction.guild))  # Вставить ID канала куда будут отправляться заявки
             await channel.send(f"(Снятие предупреждения) Администратор {interaction.author.mention} снял предупреждение пользователю {self.member.mention} в количестве {countDeleteWarn}")
         else: # Если лож
             embed = disnake.Embed(color=0x2F3136, title="Пользователя не найдено!")
@@ -43,6 +45,7 @@ class ModalDeleteWarn(disnake.ui.Modal):
                                 f"не найден в таблице."
             embed.set_thumbnail(url=interaction.author.display_avatar.url)
             await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
 class ModalWarn(disnake.ui.Modal):
     def __init__(self, member: disnake.Member):
@@ -78,7 +81,7 @@ class ModalWarn(disnake.ui.Modal):
                 embed.set_thumbnail(url=interaction.author.display_avatar.url)
                 await interaction.response.send_message(embed=embed, ephemeral=True)
                 
-                channel = interaction.guild.get_channel(await user_db.get_log_channel(interaction.guild.id))  # Вставить ID канала куда будут отправляться заявки
+                channel = interaction.guild.get_channel(await log_db.get_log_channel(interaction.guild.id))  # Вставить ID канала куда будут отправляться заявки
                 await channel.send(f"(Обновление) Администратор {interaction.author.mention} выдал предупреждение пользователю {self.member.mention} в количестве {countWarn} ( по причине: {reason})")
                 
                 if await user_db.get_user_warn_count(self.member.id) >= 3: # Опять проверяем в случае истины кикаем
@@ -94,8 +97,42 @@ class ModalWarn(disnake.ui.Modal):
             embed.set_thumbnail(url=interaction.author.display_avatar.url)
             await interaction.response.send_message(embed=embed, ephemeral=True)
             
-            channel = interaction.guild.get_channel(await user_db.get_log_channel(interaction))  # Вставить ID канала куда будут отправляться заявки
+            channel = interaction.guild.get_channel(await log_db.get_log_channel(interaction))  # Вставить ID канала куда будут отправляться заявки
             await channel.send(f"Администратор {interaction.author.mention} выдал предупреждение пользователю {self.member.mention} в количестве {countWarn} ( по причине: {reason})")
+
+
+class ModalMute(disnake.ui.Modal):
+    def __init__(self, member: disnake.Member):
+        self.member = member
+
+        components = [
+            disnake.ui.TextInput(label="Время мута (в минутах)", placeholder="Введите времяя мута",
+                                 custom_id="time"),
+            disnake.ui.TextInput(label="Причина мута", placeholder="Введите причину мута", custom_id="reason")
+        ]
+
+        title = f"Замьют пользователя {member.name}"
+
+        super().__init__(title=title, components=components, custom_id="modalMute")
+
+    async def callback(self, interaction: disnake.ModalInteraction) -> None:
+        time_str = interaction.text_values["time"]
+        reason = interaction.text_values["reason"]
+        time_minutes = int(time_str)
+
+        time = datetime.datetime.now() + datetime.timedelta(minutes=time_minutes)
+        await self.member.timeout(until=time, reason=reason)
+
+        embed = disnake.Embed(color=0x2F3136, title="Пользователь замючен!")
+        embed.description = f"{interaction.author.mention}, Вы успешно замьютили пользователя {self.member.mention} " \
+                            f"на `{time_minutes}` минут"
+        embed.set_thumbnail(url=interaction.author.display_avatar.url)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+        channel = interaction.guild.get_channel(
+            await log_db.get_log_channel(interaction.guild))  # Вставить ID канала куда будут отправляться заявки
+        await channel.send(
+            f"Администратор {interaction.author.mention} изменил имя пользователю {self.member.mention} на {new_name} ( по причине: {reason})")
 
 
 class ModalReaname(disnake.ui.Modal):
@@ -122,122 +159,8 @@ class ModalReaname(disnake.ui.Modal):
         embed.set_thumbnail(url=interaction.author.display_avatar.url)
         await interaction.response.send_message(embed=embed, ephemeral=True)
         
-        channel = interaction.guild.get_channel(await user_db.get_log_channel(interaction.guild))  # Вставить ID канала куда будут отправляться заявки
+        channel = interaction.guild.get_channel(await log_db.get_log_channel(interaction.guild))  # Вставить ID канала куда будут отправляться заявки
         await channel.send(f"Администратор {interaction.author.mention} изменил имя пользователю {self.member.mention} на {new_name} ( по причине: {reason})")
-        
-        
-class ButtonMuteViev(disnake.ui.View):
-    def __init__(self, member: disnake.Member):
-        self.member = member
-        super().__init__(timeout=None)
-
-    @disnake.ui.button(label="30м", style=disnake.ButtonStyle.danger, custom_id="btMuteMinutes")
-    async def btMute30(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(minutes=30)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 30 минут.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-
-    @disnake.ui.button(label="1ч", style=disnake.ButtonStyle.danger, custom_id="btMuteOneHours")
-    async def btMute1(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=1)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 1 час.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="2ч", style=disnake.ButtonStyle.danger, custom_id="btMuteTwoHours")
-    async def btMute2(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=2)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 2 часа.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="4ч", style=disnake.ButtonStyle.danger, custom_id="btMuteFourHours")
-    async def btMute4(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=4)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 4 часа.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="6ч", style=disnake.ButtonStyle.danger, custom_id="btMuteSixHours")
-    async def btMute6(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=6)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 6 часов.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="8ч", style=disnake.ButtonStyle.danger, custom_id="btMuteEichHours")
-    async def btMute8(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=8)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 8 часов.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="10ч", style=disnake.ButtonStyle.danger, custom_id="btMuteTheanHours")
-    async def btMute10(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=10)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 10 часов.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="12ч", style=disnake.ButtonStyle.danger, custom_id="btMuteTvelfHours")
-    async def btMute12(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        time = datetime.datetime.now() + datetime.timedelta(hours=12)
-        await self.member.timeout(until=time)
-        embed = disnake.Embed(
-            title="Пользователь замьючен",
-            description=f"Пользователь {self.member.mention} был замьючен на 12 часов.",
-            color=disnake.Color.red()
-        )
-        await interaction.response.edit_message(embed=embed, view=self)
-        
-    @disnake.ui.button(label="Размьтить", style=disnake.ButtonStyle.green, custom_id="btUnMute")
-    async def btUnMute(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        await self.member.timeout(until=None, reason=None)
-        await interaction.send(f"{self.member.mention} Размьючен", ephemeral=True)
-        
-    @disnake.ui.button(label="Назад", style=disnake.ButtonStyle.secondary)
-    async def btBack(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        await interaction.response.defer()
-        view = ButtonViev(self.member)
-        embed = disnake.Embed(
-            title="Панель управление пользователем",
-            description="**Выберите дейсвие над пользователем...**",
-            color=0xdede3e,
-        )
-        if self.member.avatar:
-            embed.set_author(name=f"Профиль - {self.member.name}", icon_url=self.member.avatar.url)
-        else:
-            embed.set_author(name=f"Профиль - {self.member.name}", icon_url=self.member.default_avatar.url)
-        await interaction.edit_original_message(embed=embed, view=view)
     
     
 class ButtonViev(disnake.ui.View):
@@ -247,20 +170,15 @@ class ButtonViev(disnake.ui.View):
 
     @disnake.ui.button(label="Мут", style=disnake.ButtonStyle.danger, custom_id="btMute")
     async def btMuteAll(self, button: disnake.ui.Button, interaction: disnake.Interaction):
-        member = self.member
-        view = ButtonMuteViev(member)
-
-        embed = disnake.Embed(title="Мут",
-            description=f"Выберите насколько замьютить пользователя {member.mention}",
-            color=0xdede3e
-        )
-
-        if member.avatar:
-            embed.set_author(name=f"Профиль - {member.name}", icon_url=member.avatar.url)
+        if not self.member:
+            await interaction.response.defer()
         else:
-            embed.set_author(name=f"Профиль - {member.name}", icon_url=member.default_avatar.url)
+            await interaction.response.send_modal(ModalMute(self.member))
 
-        await interaction.response.edit_message(embed=embed, view=view)
+    @disnake.ui.button(label="Размьтить", style=disnake.ButtonStyle.green, custom_id="btUnMute")
+    async def btUnMute(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+        await self.member.timeout(until=None, reason=None)
+        await interaction.send(f"{self.member.mention} Размьючен", ephemeral=True)
 
     @disnake.ui.button(label="Забанить", style=disnake.ButtonStyle.danger, custom_id="btBan")
     async def btBan(self, button: disnake.ui.Button, interaction: disnake.Interaction):
@@ -328,6 +246,7 @@ class Admin(commands.Cog):
         else:
             embed.set_author(name=f"Профиль - {member.name}", icon_url=member.default_avatar.url)
         await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
